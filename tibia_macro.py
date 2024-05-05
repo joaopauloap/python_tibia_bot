212c0f03f94edaab5db4ba302c0417743d317dec
João Paulo Porto <jpmail98@gmail.com>
2024-05-05 03:36:33 -0300

    v2

--- tibia_macro.py
+++ tibia_macro.py
@@ -1,20 +1,31 @@
 import ctypes
 import psutil
+import json
+import keyboard
 import time
+
+with open('config.json', 'r') as file:
+    data = json.load(file)
+
+hotkey_hp = data['hotkeys']['hp']
+hotkey_mana = data['hotkeys']['mana']
+limit_hp = data['limits']['hp'] 
+limit_mana = data['limits']['mana']
+total_mana = data['total']['mana']
+total_hp = data['total']['hp']
 
 process_name = "client.exe"
 
-hotkey_hp = 0x71    #f2
-hotkey_mana = 0x73  #f4
-
-limit_hp = 75 
-limit_mana = 50
+address_hp_current = 0x0A00DBD0     
+address_mana_current = 0x0A00E0F0
 
 
-address_hp_total = 0x0A1FDBD8 #verificar
-address_hp_current = 0x0A1FDBD0
-address_mana_total = 0x08E67320 #verificar
-address_mana_current = 0x0A1FE0F0
+def get_key_code(key_name):
+    try:
+        key_code = keyboard.key_to_scan_codes(key_name)[0]
+        return hex(key_code)
+    except:
+        return None
 
 def get_pid_by_name(process_name):
     for proc in psutil.process_iter(['pid', 'name']):
@@ -22,18 +33,14 @@ def get_pid_by_name(process_name):
             return proc.info['pid']
     return None
 
-def pressKey(key):
-    ctypes.windll.user32.keybd_event(key, 0, 0, 0)  # Pressiona a tecla
-    ctypes.windll.user32.keybd_event(key, 0, 2, 0)  # Libera a tecla
-    return None
-
 def getAddressValue(address):
     buffer = ctypes.c_int()
-    if ctypes.windll.kernel32.ReadProcessMemory(process_handle, address, ctypes.byref(buffer), ctypes.sizeof(buffer), None):
+    try:
+        ctypes.windll.kernel32.ReadProcessMemory(process_handle, address, ctypes.byref(buffer), ctypes.sizeof(buffer), None)
         return buffer.value
-    else:
+    except:
         print(f"Erro ao tentar ler o endereço {hex(address)}")
-    return buffer.value
+        return None
 
 pid = get_pid_by_name(process_name)
 
@@ -45,26 +52,41 @@ else:
     process_handle = ctypes.windll.kernel32.OpenProcess(0x0010, False, pid) #read only permission
 
     if not process_handle:
-        print("Falha ao abrir o processo")
-    else:
-        print(f"Processo {pid} aberto com sucesso: handle {process_handle}")
+        print("Falha ao abrir o processo.")
+        exit()
+    print(f"Processo {pid} aberto com sucesso.")
 
-        while True:
-            hp_total = getAddressValue(address_hp_total)
-            hp_current = getAddressValue(address_hp_current)
-            mana_total = getAddressValue(address_mana_total)
-            mana_current = getAddressValue(address_mana_current)
-            
-            if hp_current < hp_total * (limit_hp/100):
-                print("enviando hotkey_hp...")
-                pressKey(hotkey_hp)
-            
-            if mana_current < mana_total * (limit_mana/100):
-                print("enviando hotkey_mana...")
-                pressKey(hotkey_mana)
 
-            print(f"hp:{hp_current}")
-            print(f"mana:{mana_current}\n")
-            time.sleep(1)
+    #procurando address_hp_current    
+    for xbyte in range(256):
+        addrHP = (address_hp_current & 0xFF00FFFF) | (xbyte << 16)        
+        if getAddressValue(addrHP) == total_hp:
+            print(f"address_hp_current: {hex(addrHP)}")
+            address_hp_current = addrHP
+            break
+
+    #procurando address_mana_current    
+    for xbyte in range(256):
+        addrMana = (address_mana_current & 0xFF00FFFF) | (xbyte << 16)        
+        if getAddressValue(addrMana) == total_mana:
+            print(f"address_mana_current: {hex(addrMana)}")
+            address_mana_current = addrMana
+            break
+
+
+    while True:
+        hp = getAddressValue(address_hp_current)
+        mana = getAddressValue(address_mana_current)
+
+        print(f"hp:{hp}  |  mana:{mana}", end="\r", flush=True)
+        
+        if hp < total_hp * (limit_hp/100):
+            #print("enviando hotkey_hp...")
+            keyboard.press(hotkey_hp)
+        
+        if mana < total_mana * (limit_mana/100):
+            #print("enviando hotkey_mana...")
+            keyboard.press(hotkey_mana)
+
         #closes
-        ctypes.windll.kernel32.CloseHandle(process_handle)
+    ctypes.windll.kernel32.CloseHandle(process_handle)
